eb2902655346f16bb766a8f818472f185486ea26
Enrique Garduno <enriquegarduno@Macintosh.local>
2016-07-30 16:01:27 -0500

    testing

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, eaten) VALUES ('McDowell Cheesburger', FALSE);
+INSERT INTO burgers (name, eaten) VALUES ('McDowell Africanburger', FALSE);
+INSERT INTO burgers (name, eaten) VALUES ('McDowell Baconburger', FALSE);
